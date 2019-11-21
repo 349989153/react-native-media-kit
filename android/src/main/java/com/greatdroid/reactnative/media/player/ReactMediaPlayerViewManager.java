7ba45672597d91988fc9fdbf759a200613761bc1
xukj <xukj002@forsealife.com>
2019-11-21 10:41:51 +0800

    support androidx

--- android/src/main/java/com/greatdroid/reactnative/media/player/ReactMediaPlayerViewManager.java
+++ android/src/main/java/com/greatdroid/reactnative/media/player/ReactMediaPlayerViewManager.java
@@ -2,7 +2,7 @@ package com.greatdroid.reactnative.media.player;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.WindowManager;
 
